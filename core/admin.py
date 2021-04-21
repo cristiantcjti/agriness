d2bb1776ba00e5a8518d3207370f895cfc5fbf92
cristian <cristiantcjti@gmail.com>
2021-04-21 12:12:17 -0300

    Refactoring admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Client, Book, BookReservations
 
 # Register your models here.
+
+admin.site.register(Client)
+admin.site.register(Book)
+admin.site.register(BookReservations)
